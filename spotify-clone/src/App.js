9e498fba6f0d707b7f509e20e0d02fe15aa8f956
Michael <michael.s.ross10@gmail.com>
2023-02-25 00:36:27 -0700

    fixed html title name, icon and successfully created an axios call for spotify token

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,9 +1,57 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { DataContext } from './DataContext'
+import { Buffer } from 'buffer';
+import Dashboard from './components/Dashboard';
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+const auth_token = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`, 'utf-8').toString('base64')
 
 function App() {
+
+  const [ accessToken, setAccessToken ] = useState("")
+  const qs = require('qs')
+
+  const getToken = async () => {
+
+    try{
+      const token_url = "https://accounts.spotify.com/api/token"
+      const data = qs.stringify({'grant_type':'client_credentials'})
+
+      const response = await axios.post(token_url, data, {
+        headers: {
+          'Authorization': `Basic ${auth_token}`,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      })
+      setAccessToken(response.data.access_token)
+      return response.data.access_token
+    }catch(e){
+      console.log(e)
+    }
+  }
+
+  // useEffect(() => {
+  //   getToken()
+  // },[])
+
+  useEffect(()=>{
+    async function checkToken() {
+      if (accessToken === "") {
+        console.log("No established token...getting token.")
+        getToken()
+      } else {
+        console.log("Access Token is valid: ", accessToken)
+      }
+    }
+    checkToken()
+  },[accessToken])
+
   return (
     <div className="App">
-
+      <Dashboard />
     </div>
   );
 }
---
+++ spotify-clone/src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+//Dashboard will house the navigation menu and display options depending on navigation selection
+//example: if search option is selected, dashboard will render search page
+
+export default function Dashboard() {
+  return(
+    <div className="dashboard">
+      <h1>SPOTIFY</h1>
+    </div>
+  )
+}
